83ef29c8ce364eab240b26488ff47618118bbf24
Cooper <kevinjallet64@gmail.com>
2026-05-07 15:39:27 +0200

    Add files via upload

---
+++ testUnitaire.h
@@ -0,0 +1,60 @@
+#ifndef __testUnitaire_H_
+#define __testUnitaire_H_
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F1*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF1_1();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F1*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF1_25();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F1*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF1_0();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F1*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF1_10();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F2*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF2_1();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F2*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF2_25();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F2*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF2_0();
+
+/* Auteur : Kevin Jean-Paul Philippe Jallet */
+/* Date : 07/05/2026 */
+/* Résume : fonction qui test la fonction F2*/
+/* Entrée : aucune */
+/* Sortie : aucune */
+void testF2_10();
+
+#endif
