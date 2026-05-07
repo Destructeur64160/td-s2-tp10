c17faa8a4196ae111014b00a68617d8a4a67a4bc
Cooper <kevinjallet64@gmail.com>
2026-05-07 15:39:14 +0200

    Add files via upload

---
+++ testUnitaire.c
@@ -0,0 +1,83 @@
+#include "testUnitaire.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void testF1_1(){
+    int nb=1;
+    if (F1(nb)==1){
+        printf("F1(1) SUCCEED! \n");
+    }
+    else{
+        printf("F1(1) FAIL! \n");
+    }
+}
+
+void testF1_25(){
+    int nb=25;
+    if (F1(nb)==11001){
+        printf("F1(25) SUCCEED! \n");
+    }
+    else{
+        printf("F1(25) FAIL! \n");
+    }
+}
+
+void testF1_0(){
+    int nb=0;
+    if (F1(nb)==0){
+        printf("F1(0) SUCCEED! \n");
+    }
+    else{
+        printf("F1(0) FAIL! \n");
+    }
+}
+
+void testF1_10(){
+    int nb=10;
+    if (F1(nb)==1010){
+        printf("F1(10) SUCCEED! \n");
+    }
+    else{
+        printf("F1(10) FAIL! \n");
+    }
+}
+
+void testF2_1(){
+    int nb=1;
+    if (F2(nb,0)==1){
+        printf("F2(1) SUCCEED! \n");
+    }
+    else{
+        printf("F2(1) FAIL! \n");
+    }
+}
+
+void testF2_0(){
+    int nb=0;
+    if (F2(nb,0)==0){
+        printf("F2(0) SUCCEED! \n");
+    }
+    else{
+        printf("F2(0) FAIL! \n");
+    }
+}
+
+void testF2_25(){
+    int nb=25;
+    if (F2(nb,0)==11001){
+        printf("F2(25) SUCCEED! \n");
+    }
+    else{
+        printf("F2(25) FAIL! \n");
+    }
+}
+
+void testF2_10(){
+    int nb=10;
+    if (F2(nb,0)==10){
+        printf("F2(10) SUCCEED! \n");
+    }
+    else{
+        printf("F2(10) FAIL! \n");
+    }
+}
